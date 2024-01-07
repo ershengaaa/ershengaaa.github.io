4d04353736f9a8e1e3c9bf23756cf415f517b686
ershengaaa <1137228286@qq.com>
2024-01-07 18:08:21 +0800

    Modify:修复代码块显示异常问题

---
+++ libs/codeBlock/codeShowExpand.js
@@ -0,0 +1,15 @@
+// 代码块全屏显示
+
+$(function () {
+  var $code_show_expand = $('<i class="fas fa-expand code-show-expand"  title="全屏显示" aria-hidden="true"></i>');
+  $('pre').before($code_show_expand);
+  $('.code-area .code-show-expand').on('click', function (e) {
+    //if (e.target !== this) return
+    if ($(this).parent().hasClass('code-closed')) {
+      $(this).siblings('pre').find('code').show();
+      $(this).parent().removeClass('code-closed');
+    }
+    $(this).parent().toggleClass('code-block-fullscreen')
+    $('html').toggleClass('code-block-fullscreen-html-scroll')
+  });
+});
