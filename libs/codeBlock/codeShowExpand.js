a975d1691935dd276b3dea261a4d194f78ba3e72
ershengaaa <1137228286@qq.com>
2024-01-21 20:29:32 +0800

    Modify:支持label、group-image tag；更新Markdown文章

--- libs/codeBlock/codeShowExpand.js
+++ libs/codeBlock/codeShowExpand.js
@@ -4,7 +4,7 @@ $(function () {
   var $code_show_expand = $('<i class="fas fa-expand code-show-expand"  title="全屏显示" aria-hidden="true"></i>');
   $('pre').before($code_show_expand);
   $('.code-area .code-show-expand').on('click', function (e) {
-    //if (e.target !== this) return
+    if (e.target !== this) return
     if ($(this).parent().hasClass('code-closed')) {
       $(this).siblings('pre').find('code').show();
       $(this).parent().removeClass('code-closed');
